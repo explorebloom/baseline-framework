7e58d66f574d728d7b29e6c15eb6c09599ffb3bd
Terrance Woest <terrance@tremli.com>
2015-09-26 17:40:30 -0700

    Updated autoloading.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Baseline' => array($baseDir . '/framework'),
+    'Baseline\\' => array($baseDir . '/framework'),
 );
